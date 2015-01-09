783f5288d5eca1ae53e7109031d1c8e16a495e9d
Andrew <andyjacuw@gmail.com>
2015-01-09 13:09:02 -0800

    Store new todo task html in var

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $('.submit-btn').click(addNewTodo);
+
+  $('.todo-item').click(function() {
+    alert('Function called!');
+    $(this).remove();
+  });
 });
 
 function addNewTodo() {
-  var newTodoTask = $('.new-todo-task').val();
+  var newTodoTask = $('.new-todo-task').val()
+      , todoItemHTML = '<li class="todo-item">' + newTodoTask + '</li>';
 
   if (newTodoTask === '') return;
-  $('.todo-list').append('<li class="todo-item">' + newTodoTask + '</li>');
+  $('.todo-list').append(todoItemHTML);
   $('.new-todo-task').val('');
   restrictSubmit();
 }
