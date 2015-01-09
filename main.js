cfd4c38b6fafbf18b786ce8eed2db2bf552b7890
Andrew <andyjacuw@gmail.com>
2015-01-09 12:43:00 -0800

    Split click and submit event handlers into multiple functions

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-  console.log('Jquery loaded!');
+  $('.submit-btn').click(addNewTodo);
+});
 
-  $('.submit-btn').click(function() {
-    var newTodoTask = $('.new-todo-task').val();
+function addNewTodo() {
+  var newTodoTask = $('.new-todo-task').val();
 
-    if (newTodoTask === '') {
-      return;
-    }
-    $('.todo-list').append('<li class="todo-item">' + newTodoTask + '</li>');
-    $('.new-todo-task').val('');
-  });
+  if (newTodoTask === '') return;
+  $('.todo-list').append('<li class="todo-item">' + newTodoTask + '</li>');
+  $('.new-todo-task').val('');
+  restrictSubmit();
+}
 
+function restrictSubmit() {
   $('.todo-form').submit(function(e) {
     e.preventDefault();
     return;
   });
-});
+}
