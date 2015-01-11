a4ac22cd4411d82c082a46aa5c4147e091ba930b
Andrew <andyjacuw@gmail.com>
2015-01-11 12:23:22 -0800

    Use css diplay property to hide item preview on page load

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 $(document).ready(function() {
-  $('.todo-item-preview').hide();
-
   $('.new-todo-input').keyup(showItemPreview);
-
   $('.submit-btn').click(addNewTodo);
-
   $('html').on('click', '.todo-item', removeItem);
 });
 
 function showItemPreview() {
   if ($('.new-todo-input').val() !== '') {
-    $('.todo-item-preview').show();
+    $('.todo-item-preview').css('display', 'list-item');
   }
   else {
-    $('.todo-item-preview').hide();
+    $('.todo-item-preview').css('display', 'none');
   }
 
   $('.todo-item-preview').text($(this).val());
 }
 
 function addNewTodo() {
-  $('.todo-item-preview').hide();
+  $('.todo-item-preview').css('display', 'none');
   var newTodoTask = $('.new-todo-input').val()
       , todoItemHTML = '<li class="todo-item"><a href="#">' + newTodoTask + '</a></li>';
 
